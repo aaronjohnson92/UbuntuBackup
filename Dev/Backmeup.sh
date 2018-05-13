c88f30c17db90cc48f93b107534b1577074a75a8
aaronjohnson92 <38840150+aaronjohnson92@users.noreply.github.com>
2018-05-13 10:06:15 -0400

    Add files via upload

--- Dev/Backmeup.sh
+++ Dev/Backmeup.sh
@@ -259,7 +259,7 @@ Automated_Full_Home_Folder_Backup()
   time=`date +%T`
   sudo tar -cvpzf $tar_file --one-file-system $working_dir
   sudo 7zr u -up0q3r2x2y2z1w2 $zip_file $tar_file
-  # sudo rm -rf /home/Backup/user-home-folder-backup.tar
+  sudo rm -rf /home/Backup/user-home-folder-backup.tar
   echo "Full backup completed on: " $date " at: " $time
 }
 
